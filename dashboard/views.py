75609568a2582120fe73b7e84774416f37ceb82c
Jefferson Duarte <jeffersonsduarte@live.com>
2025-03-13 21:17:24 +0000

    Added filter for nutritionists to view only their patients

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,15 +7,30 @@ from authentication.models import CustomerProfile, NutritionistProfile
 def dashboard(request):
 
     if request.user.is_authenticated:
-        nutri_profile = NutritionistProfile.objects.filter(user=request.user)
-        customer_profile = CustomerProfile.objects.filter(user=request.user)
+        nutri_profile = NutritionistProfile.objects.filter(
+            user=request.user
+        ).first()
 
-        if nutri_profile.exists():
-            customers = CustomerProfile.objects.all()
-            return render(request, 'dashboard/nutritionist_dashboard.html', {'customers': customers})
+        if nutri_profile:
+            customers = CustomerProfile.objects.filter(
+                nutritionist=nutri_profile
+            )
+            return render(
+                request,
+                'dashboard/nutritionist_dashboard.html',
+                {'customers': customers}
+            )
 
-        elif customer_profile.exists():
-            return render(request, 'dashboard/customer_dashboard.html')
+        customer_profile = CustomerProfile.objects.filter(
+            user=request.user
+        ).first()
+        if customer_profile:
+            nutritionist = customer_profile.nutritionist
+            return render(
+                request,
+                'dashboard/customer_dashboard.html',
+                {'nutritionist': nutritionist}
+            )
 
     messages.error(request, 'You are not authorized to access this page')
     return redirect('login_create')
