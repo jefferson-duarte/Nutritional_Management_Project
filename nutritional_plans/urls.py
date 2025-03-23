195a9968806510b46454a49e65910e231a4af993
Jefferson Duarte <jeffersonsduarte@live.com>
2025-03-23 23:31:54 +0000

    Separate nutritional plans for each client

--- nutritional_plans/urls.py
+++ nutritional_plans/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('plan_list', views.nutritional_plan_list, name='plan_list'),
+    path('plan_list/<int:customer_id>', views.nutritional_plan_list, name='plan_list'),  # noqa:E501
     path('plan_create/<int:customer_id>', views.nutritional_plan_create, name='plan_create'),  # noqa:E501
     path('plan_edit/<int:pk>', views.nutritional_plan_edit, name='plan_edit'),
     path('plan_detail/<int:pk>', views.nutritional_plan_detail, name='plan_detail'),  # noqa:E501
--- nutritional_plans/views.py
+++ nutritional_plans/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect, render
@@ -9,9 +10,19 @@ from .models import NutritionalPlan
 
 
 @login_required
-def nutritional_plan_list(request):
-    plans = NutritionalPlan.objects.filter(nutritionist=request.user)
-    return render(request, 'nutritional_plans/plan_list.html', {'plans': plans})  # noqa:E501
+def nutritional_plan_list(request, customer_id):
+    customer = get_object_or_404(CustomerProfile, id=customer_id)
+
+    if customer.nutritionist.user != request.user:
+        return HttpResponseForbidden("You are not authorized to view this client's plans.")  # noqa:E501
+
+    plans = NutritionalPlan.objects.filter(client=customer.user)
+
+    context = {
+        'customer': customer,
+        'plans': plans,
+    }
+    return render(request, 'nutritional_plans/plan_list.html', context)
 
 
 @login_required
@@ -21,18 +32,15 @@ def nutritional_plan_create(request, customer_id=None):
     if customer_id:
         customer = get_object_or_404(CustomerProfile, id=customer_id)
 
-        if customer.nutritionist and customer.nutritionist.user != request.user:  # noqa:E501
-            return HttpResponseForbidden("You are not authorized to prescribe for this client.")  # noqa:E501
-
     if request.method == 'POST':
         form = NutritionalPlanForm(request.POST)
         if form.is_valid():
             plan = form.save(commit=False)
             plan.nutritionist = request.user
             if customer:
-                plan.customer = customer.user
+                plan.client = customer.user
             plan.save()
-            return redirect('plan_list')
+            return redirect('plan_list', customer.id)
     else:
         form = NutritionalPlanForm()
         if customer:
@@ -49,26 +57,57 @@ def nutritional_plan_create(request, customer_id=None):
 @login_required
 def nutritional_plan_edit(request, pk):
     plan = get_object_or_404(NutritionalPlan, pk=pk, nutritionist=request.user)
+
+    try:
+        customer = CustomerProfile.objects.get(user=plan.client)
+        customer_id = customer.id
+    except CustomerProfile.DoesNotExist:
+        messages.error(request, "Customer profile not found.")
+        return redirect('dashboard')
+
     if request.method == 'POST':
         form = NutritionalPlanForm(request.POST, instance=plan)
         if form.is_valid():
             form.save()
-            return redirect('plan_list')
+            return redirect('plan_list', customer_id=customer_id)
+
     else:
         form = NutritionalPlanForm(instance=plan)
-    return render(request, 'nutritional_plans/plan_form.html', {'form': form})
+
+    context = {
+        'form': form,
+        'plan': plan
+    }
+
+    return render(request, 'nutritional_plans/plan_form.html', context)
 
 
 @login_required
 def nutritional_plan_detail(request, pk):
-    plan = get_object_or_404(NutritionalPlan, pk=pk, nutritionist=request.user)
-    return render(request, 'nutritional_plans/plan_detail.html', {'plan': plan})  # noqa:E501
+    plan = get_object_or_404(NutritionalPlan, pk=pk)
+    customer = CustomerProfile.objects.get(user=plan.client)
+
+    context = {
+        'plan': plan,
+        'customer': customer  # Passa o customer para o template
+    }
+
+    return render(request, 'nutritional_plans/plan_detail.html', context)
 
 
 @login_required
 def nutritional_plan_delete(request, pk):
     plan = get_object_or_404(NutritionalPlan, pk=pk, nutritionist=request.user)
+
+    try:
+        customer = CustomerProfile.objects.get(user=plan.client)
+        customer_id = customer.id
+    except CustomerProfile.DoesNotExist:
+        messages.error(request, "Customer profile not found.")
+        return redirect('dashboard')
+
     if request.method == 'POST':
         plan.delete()
-        return redirect('plan_list')
+        return redirect('plan_list', customer_id=customer_id)
+
     return render(request, 'nutritional_plans/plan_confirm_delete.html', {'plan': plan})  # noqa:E501
