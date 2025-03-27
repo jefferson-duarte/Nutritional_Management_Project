571bbc7d1e5e6828a77c20bdc4643ae277284afb
Jefferson Duarte <jeffersonsduarte@live.com>
2025-03-27 15:47:41 +0000

    Created list of nutritionists

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('select-nutritionist/<int:nutritionist_id>/', views.select_nutritionist, name='select_nutritionist'),  # noqa:E501
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, render
 
@@ -44,14 +45,45 @@ def dashboard(request):
                 client=request.user
             )
 
+            available_nutritionists = NutritionistProfile.objects.exclude(
+                user__customerprofile__isnull=False
+            )
+
+            paginator = Paginator(available_nutritionists, 3)
+            page_number = request.GET.get('page', 1)
+            page_obj = paginator.get_page(page_number)
+
             return render(
                 request,
                 'dashboard/customer_dashboard.html',
                 {
                     'nutritionist': nutritionist,
-                    'nutritional_plans': nutritional_plans
+                    'nutritional_plans': nutritional_plans,
+                    'available_nutritionists': page_obj,
                 }
             )
 
     messages.error(request, 'You are not authorized to access this page')
     return redirect('login_create')
+
+
+@login_required
+def select_nutritionist(request, nutritionist_id):
+    user = request.user
+    customer_profile = CustomerProfile.objects.filter(user=user).first()
+
+    if not customer_profile:
+        messages.error(request, "You don't have a customer profile.")
+        return redirect('dashboard')
+
+    try:
+        nutritionist = NutritionistProfile.objects.get(id=nutritionist_id)
+        customer_profile.nutritionist = nutritionist
+        customer_profile.save()
+        messages.success(
+            request, "You have successfully selected a nutritionist."
+        )
+    except NutritionistProfile.DoesNotExist:
+        messages.error(request, "Selected nutritionist does not exist.")
+
+    return redirect('dashboard')
